da03e679a74e25f59efc46527eabc3b2b51bf7de
Muhammad Awais <mawais@updraftplus.com>
2022-09-25 01:14:37 +0500

    add singleton class

---
+++ src/Singleton.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace DSI\TechPub;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Base class for singleton objects.
+ * Class Singleton.
+ * @package DSI\TechPub
+ * @abstract
+ */
+abstract class Singleton
+{
+
+	private static $instances = array();
+	protected function __construct()
+	{
+	}
+	protected function __clone()
+	{
+	}
+	public function __wakeup()
+	{
+		throw new \Exception('Cannot unserialize singleton');
+	}
+
+
+	/**
+	 * Get instance object.
+	 **/
+	public static function get_instance()
+	{
+		$cls = get_called_class(); // late-static-bound class name
+		if (!isset(self::$instances[$cls])) {
+			self::$instances[$cls] = new static;
+			(self::$instances[$cls])->on_construct();
+		}
+		return self::$instances[$cls];
+	}
+
+	function on_construct()
+	{
+	}
+
+	/**
+	 * Returns the singleton instance.
+	 *
+	 * @return $this
+	 */
+	public static function instance()
+	{
+		return static::get_instance();
+	}
+}
