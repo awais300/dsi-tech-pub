e4d9fd68385a1219b3cd7870bd5ead498f390276
Muhammad Awais <mawais@updraftplus.com>
2022-09-25 01:17:32 +0500

    customer notice template

---
+++ templates/woocommerce/user-register-form.php
@@ -0,0 +1,8 @@
+<?php
+use DSI\TechPub\User\UserLogin; 
+?>
+<?php if(isset($_GET[UserLogin::REGISTER_STATUS_KEY]) && $_GET[UserLogin::REGISTER_STATUS_KEY] == 'pending'): ?>
+<div class="notify">
+	<p>Thank you for submitting your request. Your request has been received and will be processed as soon as possible. You will receive an email notification once your request has been approved.</p>
+</div>
+<?php endif;?>
