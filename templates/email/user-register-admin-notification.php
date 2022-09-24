42d4afbfe0d65555f48946e18b48b6b2a22e66e7
Muhammad Awais <mawais@updraftplus.com>
2022-09-25 01:16:58 +0500

    admin email notification template

---
+++ templates/email/user-register-admin-notification.php
@@ -0,0 +1,7 @@
+<p>We have a new retail customer pending approval.</p>
+<p>The customer's details are listed below. Please review.</p>
+
+<br/>
+
+<p><b>Username: </b><?php echo $username; ?></p>
+<p><b>Email: </b><?php echo $email; ?></p>
