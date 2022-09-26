70bca2af5bf810f1dd4b3cfdc224b94b27d058b9
Muhammad Awais <mawais@updraftplus.com>
2022-09-26 08:15:48 +0500

    added test code

--- templates/woocommerce/dsi-tech-pub-library.php
+++ templates/woocommerce/dsi-tech-pub-library.php
@@ -1,4 +1,6 @@
 <?php
+namespace DSI\TechPub;
+
 /**
  * Template Name: DSI Tech Pub Library
  *
@@ -11,13 +13,34 @@
 get_header();
 ?>
 
+<?php
+$products = (TechPubLib::get_instance())->get_tech_pub_products();
+?>
+
 <div id="primary" <?php astra_primary_class(); ?>>
 
         <?php astra_primary_content_top(); ?>
 
         <?php astra_content_page_loop(); ?>
 
-        The Tech Pub Libraray is coming soon!
+        <?php
+                $button_url = get_site_url() . '?quantity=1&add-to-cart=';
+                $download_url = get_site_url() . '?tech_pub_file_id=';
+                foreach($products as $product) {
+                        echo "<br/>";
+                        echo $product->get_name();
+                        echo "<br/>";
+
+                        $add_url = $button_url . $product->get_id();
+                        $id = $product->get_meta('upload_file', true);
+                        $down_url = $download_url . $id;
+                        
+                        echo "<a href='{$add_url}'>Add to Cart</a>";
+                        echo "<br/>";
+                        echo "<a href='{$down_url}'>Download File</a>";
+
+                }
+        ?>
 
         <?php astra_primary_content_bottom(); ?>
 
