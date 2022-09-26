b308b00134b073207f5783da01dfa517b6c809e0
Muhammad Awais <mawais@updraftplus.com>
2022-09-26 08:14:39 +0500

    Add order class

---
+++ src/Order.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace DSI\TechPub;
+
+use DSI\TechPub\ACF\ACF;
+use DSI\TechPub\User\UserRoles;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Class Order
+ * @package DSI\TechPub
+ */
+
+class Order extends Singleton
+{
+    /**
+     * Media ID key to store against order item.
+     *
+     * @var META_ORDER_ITEM_MEDIA_ID
+     */
+    public const META_ORDER_ITEM_MEDIA_ID = '_order_item_media_id';
+
+    /**
+     * Holds unix timestamp when access granted (product is purchased). 
+     *
+     * @var META_ORDER_ITEM_MEDIA_ACCESS_TIME
+     */
+    public const META_ORDER_ITEM_MEDIA_ACCESS_TIME = '_order_item_media_access_time';
+
+    /**
+     * Number of hours to allow for file access. 
+     *
+     * @var HOURS_ALLOWED_TO_ACCESS_FILE
+     */
+    public const HOURS_ALLOWED_TO_ACCESS_FILE = 48;
+
+    /**
+     * Allowed order statuses list.
+     *
+     * @var $allowed_order_statuses
+     */
+    public $allowed_order_statuses = array(
+        'wc-processing',
+        'wc-on-hold',
+    );
+
+    /**
+     * Construct the plugin.
+     */
+    public function __construct()
+    {
+        //add_action('init', array($this, 'test'));
+        add_action('woocommerce_checkout_create_order_line_item', array($this, 'checkout_create_order_line_item'), 20, 4);
+        add_action('woocommerce_order_item_meta_start', array($this, 'order_item_meta_start'), 20, 4);
+    }
+
+    function test()
+    {
+        if (is_admin()) {
+            return;
+        }
+        $this->has_user_access_to_media('');
+    }
+
+    /**
+     * Save media ID against each order item.
+     * 
+     **/
+    public function checkout_create_order_line_item($item, $cart_item_key, $values, $order)
+    {
+        $media_id = get_post_meta($item->get_product_id(), ACF::UPLOAD_FIELD_KEY, true);
+        if (!empty($media_id)) {
+            $item->update_meta_data(self::META_ORDER_ITEM_MEDIA_ID, $media_id);
+            $item->update_meta_data(self::META_ORDER_ITEM_MEDIA_ACCESS_TIME, time());
+        } else {
+            wc_add_notice('No media is attached to one of the product in the cart. Please contact site administrator.', 'error');
+        }
+    }
+
+    /**
+     * Check if current user has access permission to download the media.
+     * 
+     * @param  int  $product_id
+     * @return boolean
+     */
+    public function has_user_access_to_media($product_id)
+    {
+        // Retail user only can access media if they have ordered/purchased.
+        /* if ((UserRoles::get_instance())->is_retail_user() === false) {
+            return false;
+        }*/
+
+        $args = array(
+            'customer_id' => get_current_user_id(),
+            'status' => $this->allowed_order_statuses,
+            'limit' => -1,
+        );
+
+        $orders = wc_get_orders($args);
+
+        if (empty($orders)) {
+            return false;
+        }
+
+        foreach ($orders as $order) {
+            foreach ($order->get_items() as $item_id => $item) {
+                $order_item_id = $item->get_product_id();
+                $media_id_in_post = get_post_meta($order_item_id, ACF::UPLOAD_FIELD_KEY, true);
+
+                if ($order_item_id == $product_id && !empty($media_id_in_post)) {
+
+                    $unix_time = $item->get_meta(self::META_ORDER_ITEM_MEDIA_ACCESS_TIME, true);
+                    $media_id = $item->get_meta(self::META_ORDER_ITEM_MEDIA_ID, true);
+
+                    if (empty($unix_time) || empty($media_id)) {
+                        return false;
+                    }
+
+                    if ($media_id == $media_id_in_post && $this->is_access_expired($unix_time)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public function order_item_meta_start($item_id, $item, $order, $bool)
+    {
+        if (is_admin()) {
+            return;
+        }
+
+         // Retail user only can access media if they have ordered/purchased.
+        /* if ((UserRoles::get_instance())->is_retail_user() === false) {
+            return false;
+        }*/
+
+        if (is_wc_endpoint_url('view-order')) {
+            $order_item_id = $item->get_product_id();
+            $media_id_in_post = get_post_meta($order_item_id, ACF::UPLOAD_FIELD_KEY, true);
+
+            if (!empty($media_id_in_post)) {
+                $unix_time = $item->get_meta(self::META_ORDER_ITEM_MEDIA_ACCESS_TIME, true);
+                $media_id = $item->get_meta(self::META_ORDER_ITEM_MEDIA_ID, true);
+
+                if (empty($unix_time) || empty($media_id)) {
+                    return;
+                }
+
+                if ($media_id == $media_id_in_post && $this->is_access_expired($unix_time)) {
+                    // todo.
+                    // Get download button.
+                }
+            }
+        }
+    }
+
+    /**
+     * Check if hours are expired.
+     * 
+     * @param  int  $date
+     * @return boolean
+     */
+    public function is_access_expired($date)
+    {
+        $access_hours = $this->get_time_diff_in_hours($date);
+        if ($access_hours <= self::HOURS_ALLOWED_TO_ACCESS_FILE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get time difference in hours. 
+     * Takes a past $date in strtotime.
+     * 
+     * @param  int $date
+     * @return int
+     */
+    public function get_time_diff_in_hours($date)
+    {
+        $now = time();
+        $seconds = $now - $date;
+        $hours = $seconds / 60 / 60;
+        return $hours;
+    }
+}
