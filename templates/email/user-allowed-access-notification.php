e91a062d22c8fa0ef06c31959bf09056d120ddd9
Muhammad Awais <mawais@updraftplus.com>
2022-10-02 03:37:40 +0500

    update wording

--- templates/email/user-allowed-access-notification.php
+++ templates/email/user-allowed-access-notification.php
@@ -4,4 +4,4 @@ use DSI\TechPub\User\UserLogin;
 ?>
 <p>Hello <?php echo $user->user_login; ?>,</p>
 
-<p>Your access to the tech library is now granted. <a href="<?php echo get_site_url() . '/' . UserLogin::MY_ACCOUNT_PAGE; ?>"> Log in </a> and browse the library contents. You could also download tech publications if you are allowed to.</p>
+<p>Your access to the tech publication library is now granted. You can now <a href="<?php echo get_site_url() . '/' . UserLogin::MY_ACCOUNT_PAGE; ?>"> Log in </a> and can browse the library contents.</p>
