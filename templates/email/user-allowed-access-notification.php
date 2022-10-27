57acf1eab3176c9923b7a430d8c603a4ec0bb563
Muhammad Awais <mawais@updraftplus.com>
2022-10-27 23:18:56 +0500

    update email body

--- templates/email/user-allowed-access-notification.php
+++ templates/email/user-allowed-access-notification.php
@@ -4,4 +4,4 @@ use DSI\TechPub\User\UserLogin;
 ?>
 <p>Hello <?php echo $user->user_login; ?>,</p>
 
-<p>Your access to the tech publication library is now granted. You can now <a href="<?php echo get_site_url() . '/' . UserLogin::MY_ACCOUNT_PAGE; ?>"> Log in </a> and can browse the library contents.</p>
+<p>Your access to the Tech Pub Library is now granted. You can now <a href="<?php echo get_site_url() . '/' . UserLogin::MY_ACCOUNT_PAGE; ?>"> Log in </a> and can browse the library contents.</p>
